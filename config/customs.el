c161699edb455949eced704869877238518d2f60
dim3nsi4 <seimandi.pierre@gmail.com>
2017-09-10 10:16:27 +0200

    Move custom faces and variables to config/customs.el.

---
+++ config/customs.el
@@ -0,0 +1,32 @@
+;;; Package --- Summary
+;;; Emacs custom variables and faces (modified automatically by emacs).
+;;;
+;;; Commentary:
+;;; Copyright (c) 2016 Pierre Seimandi
+;;; Under GPL License v3.0 and after.
+;;;
+;;; Time-stamp: <2017-09-10 10:10:49 seimandp>
+;;;
+;;; Code:
+;;; ————————————————————————————————————————————————————————
+
+;;; -*- no-byte-compile: t -*-
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (org-bullets which-key define-word dired-narrow dired-collapse dired-sidebar beacon use-package-chords modalka flyspell-correct-ivy org helm dokuwiki-mode dokuwiki mmm-mode all-the-icons anzu avy cdlatex company-auctex company-bibtex company-jedi company-quickhelp counsel counsel-gtags counsel-projectile crux diff-hl diminish dired-subtree drag-stuff expand-region eyebrowse flx gnuplot gnuplot-mode google-c-style help-fns+ hl-anything hydra image+ ivy ivy-hydra ivy-rich java-snippets lua-mode magithub markdown-mode matlab-mode meghanada multiple-cursors neotree org-ref origami paradox pdf-tools perspeen popwin rainbow-mode req-package smartparens smex spaceline all-the-icons-dired sqlup-mode undo-tree use-package vimish-fold volatile-highlights whitespace-cleanup-mode zzz-to-char))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;; ————————————————————————————————————————————————————————
+;;; customs.el ends here
--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;;; Copyright (c) 2016 Pierre Seimandi
 ;;; Under GPL License v3.0 and after.
 ;;;
-;;; Time-stamp: <2017-09-09 20:22:48 seimandp>
+;;; Time-stamp: <2017-09-10 10:14:04 seimandp>
 ;;;
 ;;; Code:
 ;;; ————————————————————————————————————————————————————————
@@ -97,23 +97,8 @@
 
 (req-package-finish)
 
-;;; ********************************************************
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (org-bullets which-key define-word dired-narrow dired-collapse dired-sidebar beacon use-package-chords modalka flyspell-correct-ivy org helm dokuwiki-mode dokuwiki mmm-mode all-the-icons anzu avy cdlatex company-auctex company-bibtex company-jedi company-quickhelp counsel counsel-gtags counsel-projectile crux diff-hl diminish dired-subtree drag-stuff expand-region eyebrowse flx gnuplot gnuplot-mode google-c-style help-fns+ hl-anything hydra image+ ivy ivy-hydra ivy-rich java-snippets lua-mode magithub markdown-mode matlab-mode meghanada multiple-cursors neotree org-ref origami paradox pdf-tools perspeen popwin rainbow-mode req-package smartparens smex spaceline all-the-icons-dired sqlup-mode undo-tree use-package vimish-fold volatile-highlights whitespace-cleanup-mode zzz-to-char))))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file  (expand-file-name "config/customs.el" emacs-d))
+(load custom-file)
 
 (provide 'init)
 
