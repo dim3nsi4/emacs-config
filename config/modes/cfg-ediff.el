f8d7a02a12cb26e88e9f0323a550578d58fec46e
dim3nsi4 <seimandi.pierre@gmail.com>
2017-09-10 13:13:53 +0200

    [cfg-ediff.el] Remove ediff-directories-recursive (eval error).

--- config/modes/cfg-ediff.el
+++ config/modes/cfg-ediff.el
@@ -48,55 +48,6 @@
   (add-hook 'ediff-after-setup-windows-hook 'my/ediff-ash 'append)
   (add-hook 'ediff-quit-hook 'my/ediff-qh))
 
-;; --
-
-(eval
- (let ((directory-files-original (symbol-function 'directory-files)))
-
-
-   `(defun my/directory-files-recursive (directory &optional full match nosort)
-      "Like `directory-files' but recurses into subdirectories. Does not follow symbolic links."
-      (let* ((prefix (or (and full "") directory))
-             dirs
-             files)
-        (mapc (lambda (p)
-                (let ((fullname (if full p (concat prefix "/" p))))
-                  (when (and (file-directory-p fullname)
-                             (null (or (string-match "\\(^\\|/\\).$" p)
-                                       (string-match "\\(^\\|/\\)..$" p)
-                                       (file-symlink-p fullname))))
-                    (setq dirs (cons p dirs)))))
-              (funcall ,directory-files-original directory full nil nosort))
-        (setq dirs (nreverse dirs))
-        (mapc (lambda (p)
-                (when (null (file-directory-p (if full p (concat prefix "/" p))))
-                  (setq files (cons p files))))
-              (funcall ,directory-files-original directory full match nosort))
-        (setq files (nreverse files))
-        (mapc (lambda (d)
-                (setq files
-                      (append files
-                              (if full
-                                  (apply 'my/directory-files-recursive (list d full match nosort))
-                                (mapcar (lambda (n)
-                                          (concat d "/" n))
-                                        (apply 'my/directory-files-recursive (list (concat prefix "/" d) full match nosort)))))))
-              dirs)
-        files))))
-
-(eval
- `(defun ediff-directories-recursive (dir1 dir2 regexp)
-    "Like `ediff-directories' but recurses into sub-directories. Does not follow symbolic links."
-    ,(interactive-form (symbol-function 'ediff-directories))
-    (let ((directory-files-original (symbol-function 'directory-files)))
-      (unwind-protect
-          (progn
-            (fset 'directory-files (symbol-function 'my/directory-files-recursive))
-            (ediff-directories dir1 dir2 regexp)
-            (fset 'directory-files directory-files-original))))))
-
-;; --
-
 (provide 'cfg-ediff)
 
 ;;; ————————————————————————————————————————————————————————
