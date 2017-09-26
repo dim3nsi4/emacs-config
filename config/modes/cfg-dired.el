f85e37d9182dfc24b88787e94c788c87c57fbdbb
dim3nsi4 <seimandi.pierre@gmail.com>
2017-09-26 20:33:15 +0200

    [cfg-dired.el] Update.

--- config/modes/cfg-dired.el
+++ config/modes/cfg-dired.el
@@ -15,22 +15,28 @@
 
   :bind
   (:map dired-mode-map
-        ("<mouse-2>" . dired-find-alternate-file)
-        ("<C-down>"  . dired-find-alternate-file)
-        ("RET"       . dired-find-alternate-file)
-        ("°"         . dired-diff)
-        ("="         . my/dired-ediff-files))
+        ("<mouse-2>"    . dired-find-alternate-file)
+        ("<C-down>"     . dired-find-alternate-file)
+        ("RET"          . dired-find-alternate-file)
+        ("°"            . dired-diff)
+        ("="            . my/dired-ediff-files)
+        ("<C-up>"       . my/dired-up-directory)
+        ("<backspace>>" . my/dired-up-directory)
+        ("<^>"          . my/dired-up-directory))
 
   :init
+  (add-hook 'dired-mode-hook #'dired-hide-details-mode)
+  (add-hook 'dired-mode-hook #'auto-revert-mode)
   (add-hook 'dired-mode-hook #'my/rename-dired-buffer-name)
-  (add-hook 'dired-mode-hook (lambda ()
-                               (define-key dired-mode-map (kbd "<C-up>")      (lambda () (interactive) (find-alternate-file "..")))
-                               (define-key dired-mode-map (kbd "<backspace>") (lambda () (interactive) (find-alternate-file "..")))
-                               (define-key dired-mode-map (kbd "^")           (lambda () (interactive) (find-alternate-file "..")))))
 
   :config
   ;; enable dired-find-alternate-file command
   (put 'dired-find-alternate-file 'disabled nil)
+
+  (defun my/dired-up-directory ()
+    "Use find-alternate-file to open the parent directory."
+    (interactive)
+    (find-alternate-file ".."))
 
   (defun my/dired-ediff-files ()
     (interactive)
@@ -60,32 +66,25 @@
           (rename-buffer (concat "*Dired* " name "/") t))))
 
   (defun my/dired-do-eshell-command (command)
-  "Run an Eshell COMMAND on the marked files."
-  (interactive "sEshell command: ")
-  (let ((files (dired-get-marked-files t)))
-    (eshell-command
-     (format "%s %s" command (mapconcat #'identity files " ")))))
+    "Run an Eshell COMMAND on the marked files."
+    (interactive "sEshell command: ")
+    (let ((files (dired-get-marked-files t)))
+      (eshell-command
+       (format "%s %s" command (mapconcat #'identity files " ")))))
 
-  (setq dired-listing-switches "-Alvh1 --group-directories-first --dereference")
-  (setq wdired-allow-to-change-permissions t)
-  (setq directory-free-space-args "-Pmh")
-  (setq dired-recursive-copies 'always)
-  (setq dired-recursive-deletes 'always)
-  (setq dired-dwim-target t)
-
-  (add-hook 'dired-mode-hook 'dired-hide-details-mode)
-  (add-hook 'dired-mode-hook 'auto-revert-mode))
+  (setq dired-listing-switches "-Alvh1 --group-directories-first --dereference"
+        wdired-allow-to-change-permissions t
+        directory-free-space-args "-Pmh"
+        dired-recursive-copies 'always
+        dired-recursive-deletes 'always
+        dired-dwim-target t))
 
 ;; ——
 
 (use-package dired-x
   :defer t
   :after dired
-  ;; :bind
-  ;; (:map dired-mode-map
-  ;;       ("z" . dired-omit-mode))
   :config
-  ;; (setq-default dired-omit-files-p t)
   (setq dired-omit-verbose nil)
   (setq dired-omit-files
         (format "\\(?:\\.%s\\'\\)\\|%s\\|\\`\\.[^.]"
@@ -99,7 +98,8 @@
 (use-package dired-filter
   :defer t
   :after dired
-  :init
+  :config
+  (setq dired-filter-inherit-filter-stack t)
   (add-hook 'dired-mode-hook 'dired-filter-mode))
 
 ;; ——
@@ -157,18 +157,12 @@
   :bind
   (("<f10>" . dired-sidebar-toggle-sidebar))
 
-  :init
-  (add-hook 'dired-sidebar-mode-hook (lambda ()
-                                       (define-key dired-sidebar-mode-map (kbd "^")           'dired-sidebar-up-directory)
-                                       (define-key dired-sidebar-mode-map (kbd "<backspace>") 'dired-sidebar-up-directory)
-                                       (define-key dired-sidebar-mode-map (kbd "<C-up>")      'dired-sidebar-up-directory)
-                                       (define-key dired-sidebar-mode-map (kbd "<C-down>")    'dired-sidebar-find-file)
-                                       (define-key dired-sidebar-mode-map (kbd "RET")         'dired-sidebar-find-file)))
-
   :config
   (setq dired-sidebar-width 35)
   (add-to-list 'dired-sidebar-special-refresh-commands 'dired-subtree-cycle)
-  (add-to-list 'dired-sidebar-special-refresh-commands 'dired-subtree-toggle))
+  (add-to-list 'dired-sidebar-special-refresh-commands 'dired-subtree-toggle)
+  (define-key dired-sidebar-mode-map [remap dired-find-alternate-file] 'dired-sidebar-find-file)
+  (define-key dired-sidebar-mode-map [remap my/dired-up-directory] 'dired-sidebar-up-directory))
 
 ;; ——
 
@@ -177,7 +171,11 @@
   :after dired
   :require dired
   :config
-  (setq dired-open-extensions '(("html" . "firefox")
+  (define-key dired-mode-map [remap dired-find-file] 'dired-open-file)
+  (define-key dired-mode-map [remap dired-find-alternate-file] 'dired-open-file)
+  (setq dired-open-find-file-function 'dired-find-alternate-file)
+  (setq dired-open-extensions '(("pdf"  . "evince")
+                                ("html" . "firefox")
                                 ( "mp4" . "smplayer")
                                 ( "mkv" . "smplayer")
                                 ( "avi" . "smplayer"))))
