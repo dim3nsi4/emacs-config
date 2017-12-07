ed8d260e81153cb7d466996df1219d40326c21a5
dim3nsi4 <seimandi.pierre@gmail.com>
2017-12-07 22:52:21 +0100

    Cleans up customs and sort the fields.

--- config/customs.el
+++ config/customs.el
@@ -17,23 +17,24 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ag all-the-icons all-the-icons-dired anzu auto-compile avy
-     cdlatex company-auctex company-bibtex company-jedi
-     company-quickhelp counsel counsel-gtags counsel-projectile
-     crux define-word diff-hl diminish dired-avfs dired-collapse
+    (ag all-the-icons all-the-icons-dired anzu auctex
+     auto-compile autodisass-java-bytecode avy cdlatex
+     company-auctex company-bibtex company-jedi company-quickhelp
+     counsel counsel-gtags counsel-projectile crux define-word
+     diff-hl diminish dired-avfs dired-collapse dired-du
      dired-filter dired-narrow dired-open dired-ranger
      dired-sidebar dired-subtree dokuwiki dokuwiki-mode
-     drag-stuff expand-region eyebrowse flx flycheck-pos-tip
-     flyspell-correct-ivy gnuplot gnuplot-mode google-c-style
-     gradle-mode groovy-mode helm help-fns+ helpful
-     company-auctex auctex ivy-xref ag all-the-icons
-     highlight-symbol hl-anything hydra image+ ivy ivy-purpose
-     ivy-rich java-snippets lua-mode magithub markdown-mode
-     matlab-mode meghanada multiple-cursors org org-bullets
-     org-ref origami paradox pdf-tools popwin rainbow-mode
-     smartparens smex sort-words sqlup-mode undo-tree use-package
-     use-package-chords vimish-fold volatile-highlights wgrep
-     which-key whitespace-cleanup-mode ztree zzz-to-char))))
+     drag-stuff ensime expand-region eyebrowse flx
+     flycheck-pos-tip flyspell-correct-ivy gnuplot gnuplot-mode
+     google-c-style gradle-mode groovy-mode helm help-fns+
+     helpful highlight-symbol hl-anything hydra image+ ivy
+     ivy-rich ivy-xref java-snippets lua-mode magithub
+     markdown-mode matlab-mode meghanada multiple-cursors org
+     org-bullets org-ref origami paradox pdf-tools popwin
+     rainbow-mode smartparens smex sort-words sqlup-mode
+     undo-tree use-package use-package-chords vimish-fold
+     volatile-highlights wgrep which-key whitespace-cleanup-mode
+     ztree zzz-to-char))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
