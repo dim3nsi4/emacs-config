26240f3dd261d5c65f28f71a91c68c2daaef7dfe
dim3nsi4 <seimandi.pierre@gmail.com>
2017-12-07 22:53:14 +0100

    Adds autodisass-java-bytecode support.

---
+++ config/modes/cfg-autodisass-java-bytecode.el
@@ -0,0 +1,17 @@
+;;; Package --- Summary
+;;; autodisass-java-bytecode configuration.
+;;;
+;;; Commentary:
+;;; Copyright (c) 2016-2017 Pierre Seimandi
+;;; Under GPL License v3.0 and after.
+;;;
+;;; Code:
+;;; ————————————————————————————————————————————————————————
+
+(use-package autodisass-java-bytecode
+  :defer t)
+
+(provide 'cfg-autodisass-java-bytecode)
+
+;;; ————————————————————————————————————————————————————————
+;;; cfg-autodisass-java-bytecode.el ends here
