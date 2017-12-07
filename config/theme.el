90efab312bd2693718be781b713361391b0d43ec
dim3nsi4 <seimandi.pierre@gmail.com>
2017-12-07 22:54:04 +0100

    Adds file for theme loading.

---
+++ config/theme.el
@@ -0,0 +1,17 @@
+;;; Package --- Summary
+;;; Theme definition.
+;;;
+;;; Commentary:
+;;; Copyright (c) 2016-2017 Pierre Seimandi
+;;; Under GPL License v3.0 and after.
+;;;
+;;; Code:
+;;; ————————————————————————————————————————————————————————
+
+;; Load the theme
+(load-theme 'clearview-light t)
+
+(provide 'theme)
+
+;;; ————————————————————————————————————————————————————————
+;;; theme.el ends here
