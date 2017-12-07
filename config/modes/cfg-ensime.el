da7a313c2f17f43581d1d7d02deee4818298dd0c
dim3nsi4 <seimandi.pierre@gmail.com>
2017-12-07 22:53:36 +0100

    Adds ensime support.

---
+++ config/modes/cfg-ensime.el
@@ -0,0 +1,21 @@
+;;; Package --- Summary
+;;; ensime configuration.
+;;;
+;;; Commentary:
+;;; Copyright (c) 2016-2017 Pierre Seimandi
+;;; Under GPL License v3.0 and after.
+;;;
+;;; Code:
+;;; ————————————————————————————————————————————————————————
+
+(use-package ensime
+  :ensure t)
+
+(use-package sbt-mode)
+
+(use-package scala-mode)
+
+(provide 'cfg-ensime)
+
+;;; ————————————————————————————————————————————————————————
+;;; cfg-ensime.el ends here
