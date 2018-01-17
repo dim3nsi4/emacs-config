6429ec51ade75fbc172236e1368f14589930eb68
dim3nsi4 <seimandi.pierre@gmail.com>
2018-01-17 23:16:34 +0100

    Adds config file for ialign

---
+++ config/modes/cfg-ialign.el
@@ -0,0 +1,18 @@
+;;; Package --- Summary
+;;; ialign configuration.
+;;;
+;;; Commentary:
+;;; Copyright (c) 2016-2017 Pierre Seimandi
+;;; Under GPL License v3.0 and after.
+;;;
+;;; Code:
+;;; ————————————————————————————————————————————————————————
+
+(provide 'cfg-ialign)
+
+(use-package ialign
+  :bind
+  (("<S-f6>" . ialign-interactive-align)))
+
+;;; ————————————————————————————————————————————————————————
+;;; cfg-ialign.el ends here
