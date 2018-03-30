c3fff9d06e58958101e4f1ad47062514df63722f
dim3nsi4 <seimandi.pierre@gmail.com>
2018-03-30 17:52:09 +0200

    [init.el] Adds https://orgmode.org/elpa/ to package-archives

--- init.el
+++ init.el
@@ -33,6 +33,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("gnu"   . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("org"   . "https://orgmode.org/elpa/") t)
 (package-initialize)
 
 (require 'cfg-use-package)
